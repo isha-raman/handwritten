08535d06997ada91089e86dc3a6af6b803021100
Isha Raman <102151133+isha-raman@users.noreply.github.com>
2022-03-23 13:27:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import pywhatkit
+text=input("enter the text: ")
+pywhatkit.text_to_handwriting(text, rgb=[0,0,250])
+
+#question=input("enter the question here: ")
+#answer=input("enter thr answer here: ")
+#pywhatkit.text_to_handwriting("Question:- "+question+"\n"+"Answer:- "+answer, rgb=[0,0,250])
